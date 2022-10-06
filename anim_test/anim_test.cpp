6d6b8e4eefc23ea82c5264e20ddbfaa9c3b720cb
Oleksandr Kapara <a.kapara2006@gmail.com>
2022-10-06 11:32:57 +0300

    Добавьте файлы проекта.

---
+++ anim_test/anim_test.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    cout << "Loading Please Wait";
+    while (true)
+    {
+        Sleep(100);
+        cout << "." << flush;
+        Sleep(100);
+        cout << "." << flush;
+        Sleep(100);
+        cout << "." << flush;
+        Sleep(100);
+        cout << "\b\b\b   \b\b\b" << flush;
+    }
+}
